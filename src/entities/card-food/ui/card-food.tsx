bf415b3379bd359ca94183436350557ca6048b65
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-05-19 01:49:48 +0300

    style: add hover transform to image

--- src/entities/card-food/ui/card-food.tsx
+++ src/entities/card-food/ui/card-food.tsx
@@ -3,8 +3,8 @@ import { Button } from '@shared/ui/button'
 export const CardFood = ({ food }) => {
   return (
     <div className="flex h-full flex-col items-center">
-      <div className="mb-2">
-        <img src="/images/pizza.png" alt="pizza" />
+      <div className="mb-2 h-full transition-transform hover:translate-y-2">
+        <img src={food.image ? food.image : '/images/pizza.png'} alt="pizza" />
       </div>
       <h3 className="mb-3.5 text-3xl font-extrabold text-gray-200">
         {food.name}

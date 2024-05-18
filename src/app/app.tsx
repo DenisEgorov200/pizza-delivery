d8697bee1c211ec5c60b55c046c07bdc60477dbe
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-05-18 22:52:21 +0300

    feat: add footer

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,11 @@
+import { Footer } from '@widgets/footer'
 import { Header } from '@widgets/header'
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }

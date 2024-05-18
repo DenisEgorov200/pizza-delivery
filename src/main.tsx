a8150774ffdb5cf4d282670dfa1ac72957e4359a
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-05-19 00:57:55 +0300

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./app/app.tsx";
-import "./app/styles/index.css";
+import './app/styles/index.css'
+import { appStarted } from '@shared/config/init.ts'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './app/app.tsx'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+appStarted()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
---
+++ src/shared/config/routes.ts
@@ -0,0 +1,25 @@
+import { createHistoryRouter, createRoute } from 'atomic-router'
+import { sample } from 'effector'
+import { createBrowserHistory } from 'history'
+import { appStarted } from './init'
+
+export const routes = {
+  catalog: createRoute(),
+}
+
+export const mappedRoutes = [
+  {
+    route: routes.catalog,
+    path: '/catalog',
+  },
+]
+
+export const router = createHistoryRouter({
+  routes: mappedRoutes,
+})
+
+sample({
+  clock: appStarted,
+  fn: () => createBrowserHistory(),
+  target: router.setHistory,
+})

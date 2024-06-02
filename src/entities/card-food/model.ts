55df85cedbf09fbb4ecd6aca6c3c028aa865b3f7
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-02 19:24:23 +0300

    feat: add food to cart

--- src/entities/card-food/model.ts
+++ src/entities/card-food/model.ts
@@ -6,3 +6,4 @@ export const foodRemoved = createEvent()
 export const $foods = createStore([])
 
 $foods.on(foodAdded, (state, food) => [...state, food])
+$foods.on(foodRemoved, (state, food) => state.filter((id) => food.id === id))
--- src/entities/card-food/ui/card-food.tsx
+++ src/entities/card-food/ui/card-food.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@shared/ui/button'
 import { useUnit } from 'effector-react'
-import { foodAdded } from '../model'
+import { useState } from 'react'
+import { foodAdded, foodRemoved } from '../model'
 
 interface Props {
   food: {
@@ -13,7 +14,19 @@ interface Props {
 }
 
 export const CardFood = ({ food }: Props) => {
-  const handleFoodAdded = useUnit(foodAdded)
+  const [handleFoodAdded, handleFoodRemoved] = useUnit([foodAdded, foodRemoved])
+
+  const [isAdded, setIsAdded] = useState(false)
+
+  const handleClick = (food) => {
+    setIsAdded(!isAdded)
+
+    if (isAdded) {
+      handleFoodRemoved(food.id)
+    } else {
+      handleFoodAdded(food)
+    }
+  }
 
   return (
     <div className="flex h-full flex-col items-center">
@@ -26,7 +39,9 @@ export const CardFood = ({ food }: Props) => {
       <p className="mb-5 font-medium text-gray-100">{food.desc}</p>
       <div className="mt-auto flex w-full items-center justify-between">
         <span className="text-2xl font-bold">от {food.price} ₽</span>
-        <Button onClick={() => handleFoodAdded(food)}>В корзину</Button>
+        <Button onClick={() => handleClick(food)}>
+          {isAdded ? 'Из корзины' : 'В корзину'}
+        </Button>
       </div>
     </div>
   )
